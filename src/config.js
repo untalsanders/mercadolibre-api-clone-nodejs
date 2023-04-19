987168776e1f1925f673e95df813665a10691b78
Sanders Gutiérrez <ing.sanders@gmail.com>
2023-04-19 18:57:35 -0300

    chore: change variable name to mongo URI
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ const config = {
     ENV: process.env.NODE_ENV ?? 'production',
     HOST: process.env.HOST ?? 'localhost',
     PORT: process.env.PORT ?? 3000,
-    DB_URI: process.env.DB_URI ?? 'mongodb://127.0.0.1:27017/products-api-development',
+    MONGODB_URI: process.env.MONGODB_URI ?? 'mongodb://127.0.0.1:27017/products-api-development',
     BASE_URL: process.env.BASE_URL ?? 'http://localhost:3000',
 }
 
