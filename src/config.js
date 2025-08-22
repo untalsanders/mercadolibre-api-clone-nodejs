92c4d7e33ac456b26942ecf5f36af6daa43b5e04
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-08-22 15:26:35 -0300

    fix: update connection parameters for the database mongo

--- src/config.js
+++ src/config.js
@@ -2,64 +2,101 @@
 
 import dotenv from 'dotenv'
 
-const MONGODB_NAME = process.env.NODE_ENV !== 'production' ? 'db-ml-api-clone-dev' : 'db-ml-api-clone'
-
-if (process.env.APP_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
   dotenv.config()
 }
 
 const config = {
   /*
-    |--------------------------------------------------------------------------
-    | Application Name
-    |--------------------------------------------------------------------------
-    |
-    | This value is the name of your application. This value is used when the
-    | framework needs to place the application's name in a notification or
-    | any other location as required by the application or its packages.
-    |
-    */
-  name: process.env.APP_NAME ?? 'Application',
+  |--------------------------------------------------------------------------
+  | Application Name
+  |--------------------------------------------------------------------------
+  |
+  | This value is the name of your application. This value is used when the
+  | framework needs to place the application's name in a notification or
+  | any other location as required by the application or its packages.
+  |
+  */
+  name: process.env.API_NAME ?? 'Application',
 
   /*
-    |--------------------------------------------------------------------------
-    | Application Environment
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services the application utilizes. Set this in your ".env" file.
-    |
-    */
-  env: process.env.APP_ENV ?? 'production',
+  |--------------------------------------------------------------------------
+  | Application Environment
+  |--------------------------------------------------------------------------
+  |
+  | This value determines the "environment" your application is currently
+  | running in. This may determine how you prefer to configure various
+  | services the application utilizes. Set this in your ".env" file.
+  |
+  */
+  env: process.env.NODE_ENV ?? 'development',
 
   /*
-    |--------------------------------------------------------------------------
-    | Application Debug Mode
-    |--------------------------------------------------------------------------
-    |
-    | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within your
-    | application. If disabled, a simple generic error page is shown.
-    |
-    */
-
-  debug: process.env.APP_DEBUB ?? false,
+  |--------------------------------------------------------------------------
+  | Application Debug Mode
+  |--------------------------------------------------------------------------
+  |
+  | When your application is in debug mode, detailed error messages with
+  | stack traces will be shown on every error that occurs within your
+  | application. If disabled, a simple generic error page is shown.
+  |
+  */
+  debug: process.env.API_DEBUB ?? false,
 
   /*
-    |--------------------------------------------------------------------------
-    | Application URL
-    |--------------------------------------------------------------------------
-    |
-    | This URL is used by the console to properly generate URLs when using
-    | the Artisan command line tool. You should set this to the root of
-    | your application so that it is used when running Artisan tasks.
-    |
-    */
-  url: process.env.APP_URL ?? 'http://localhost',
+  |--------------------------------------------------------------------------
+  | Application API_URL
+  |--------------------------------------------------------------------------
+  |
+  | This URL is used by the console to properly generate URLs when using
+  | the Nodemon command line tool. You should set this to the root of
+  | your application so that it is used when running Nodemon.
+  |
+  */
+  url: process.env.API_URL ?? 'http://localhost',
+
+  /*
+  |--------------------------------------------------------------------------
+  | Application HOST
+  |--------------------------------------------------------------------------
+  |
+  | This URL is used by the console to properly generate URLs when using
+  | the Artisan command line tool. You should set this to the root of
+  | your application so that it is used when running Artisan tasks.
+  |
+  */
   host: process.env.HOST ?? '127.0.0.1',
+
+  /*
+  |--------------------------------------------------------------------------
+  | Application PORT
+  |--------------------------------------------------------------------------
+  |
+  | This URL is used by the console to properly generate URLs when using
+  | the Artisan command line tool. You should set this to the root of
+  | your application so that it is used when running Artisan tasks.
+  |
+  */
   port: process.env.PORT ?? 3000,
-  mongodb_uri: process.env.MONGODB_URI ?? `mongodb://127.0.0.1:27017/${MONGODB_NAME}`,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Database URL
+  |--------------------------------------------------------------------------
+  |
+  | This value is the name of your database. This value is used when the
+  | application needs to place the database's name in a notification or
+  | any other location as required by the application or its packages.
+  |
+  */
+  database: {
+    engine: process.env.DATABASE_ENGINE ?? 'mongodb',
+    host: process.env.DATABASE_HOST ?? '127.0.0.1',
+    port: process.env.DATABASE_PORT ?? 27017,
+    user: process.env.DATABASE_USER ?? 'root',
+    pass: process.env.DATABASE_PASS ?? '12345',
+    name: process.env.DATABASE_NAME ?? 'mydb',
+  },
 }
 
 export default config
--- src/database.js
+++ src/database.js
@@ -5,15 +5,14 @@ import mongoose from 'mongoose'
 import config from './config.js'
 
 const { connect, connection } = mongoose
+
+const url = `${config.database.engine}://${config.database.user}:${config.database.pass}@${config.database.host}:${config.database.port}/${config.database.engine === 'mongodb' ? 'authMechanism=SCRAM-SHA-256' : ''}`
+const options = { dbName: config.database.name }
+
 mongoose.set('strictQuery', false)
 
-const mongooseOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}
-
-connect(config.mongodb_uri, mongooseOptions)
-  .then(() => console.log(`${chalk.black.bold.bgWhite(' DB ')} ${chalk.green(' Connection has been successfully! ')}`))
+await connect(url, options)
+  .then(() => console.log(`${chalk.black.bold.bgWhite(' DB ')} ${chalk.green(` Connection has been successfully! `)}`))
   .catch(err => console.log(`${chalk.white.bold.bgRed(' ERROR ')} in initial connection -> ${chalk.red(err.message)}`))
 
 if (config.env !== 'production') {
